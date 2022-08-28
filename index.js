6b7f3f4e28bbe2afd9f8d8f178e6fcd0dbf0302f
zhiktang <zhikai.tang123@gmail.com>
2022-08-28 01:52:01 -0500

    shows username after you type it in

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ submit.addEventListener('click', ()=> {
     var username = document.getElementById('username-input').value;
     document.getElementById('opener').style.visibility = 'hidden';
     console.log(username);
-    document.getElementById('sus').innerHTML = username;
-    document.getElementById('sus').style.visibility = 'visible';
+    document.getElementById('username-display').innerHTML = username;
+    document.getElementById('sus-surround').hidden = false;
+    document.getElementById('sus').hidden = false;
+    document.getElementById('sus-surround').style.zIndex = '1';
+    console.log("script works lol");
 });
